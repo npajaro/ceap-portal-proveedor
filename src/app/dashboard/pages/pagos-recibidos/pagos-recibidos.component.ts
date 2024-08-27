38031d71f06f7352e11435f8b791374f891efccb
npajaro <npajaro1@gmail.com>
2024-08-26 22:23:34 -0500

    feat: Agregar MatCard y MatIconModule al componente pagos-recibidos.component.ts

--- src/app/dashboard/pages/pagos-recibidos/pagos-recibidos.component.ts
+++ src/app/dashboard/pages/pagos-recibidos/pagos-recibidos.component.ts
@@ -1,11 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatCard } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-pagos-recibidos',
   standalone: true,
   imports: [
     CommonModule,
+    MatIconModule,
+    MatCard,
   ],
   templateUrl: './pagos-recibidos.component.html',
   styleUrl: './pagos-recibidos.component.css',
